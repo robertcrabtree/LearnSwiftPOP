e8060cbf86cf8dd87aab1c813c33a6561fc62f39
Robert Crabtree <robert.crabtree@gmail.com>
2017-11-13 16:24:39 -0800

    Add gesture recognizer

--- Pop/Pop/ViewController.swift
+++ Pop/Pop/ViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    lazy var tapGestureRecognizer: UITapGestureRecognizer = {
+        let tapper = UITapGestureRecognizer()
+        tapper.addTarget(self, action: #selector(viewTapped(sender:)))
+        return tapper
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func viewTapped(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
     }
 }
 
